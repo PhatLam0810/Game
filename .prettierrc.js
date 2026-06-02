626c0db3275419e1141439188b5cfc77e85fe378
PhatLT <lamtanphat56@gmail.com>
2026-06-02 14:05:46 +0700

    createGameScreen

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
---
+++ app/screens/gameScreen/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+function GameScreen() {
+  return <div>GameScreen</div>;
+}
+
+export default GameScreen;
